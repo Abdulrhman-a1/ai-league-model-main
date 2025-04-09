71ec2ece05df25803b0bab787a2eacb9a5cb4fd0
Abdulrhman-a1 <Abdulrhman-a1@outlook.com>
2025-04-10 01:14:27 +0300

    lease enter the commit message for your changes. Lines starting

--- model.py
+++ model.py
@@ -1,11 +1,15 @@
 import torch.nn as nn
 import torchvision.models as models
 
+import torch.nn as nn
+import torchvision.models as models
+from torchvision.models import VGG16_BN_Weights
+
 class CSRNet(nn.Module):
     def __init__(self):
         super(CSRNet, self).__init__()
-        from torchvision.models import VGG16_BN_Weights
-vgg = models.vgg16_bn(weights=VGG16_BN_Weights.DEFAULT)
+        vgg = models.vgg16_bn(weights=VGG16_BN_Weights.DEFAULT)
+
         self.frontend = nn.Sequential(*list(vgg.features.children())[:33])
         self.backend = nn.Sequential(
             nn.Conv2d(512, 512, 3, padding=2, dilation=2),
